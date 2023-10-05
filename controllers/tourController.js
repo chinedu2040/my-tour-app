c5b5ac436819232a547a27d9a4e160492d4630a7
chinedu2040 <udezechinedu2040@gmail.com>
2023-10-05 21:37:31 +0100

    Removed stats features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -51,51 +51,51 @@ exports.deleteTour = factory.deleteOne(Tour);
 // });
 
 
-exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
-  const year = req.params.year * 1; // 2021
+// exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+//   const year = req.params.year * 1; // 2021
 
-  const plan = await Tour.aggregate([
-    {
-      $unwind: '$startDates'
-    },
-    {
-      $match: {
-        startDates: {
-          $gte: new Date(`${year}-01-01`),
-          $lte: new Date(`${year}-12-31`)
-        }
-      }
-    },
-    {
-      $group: {
-        _id: { $month: '$startDates' },
-        numTourStarts: { $sum: 1 },
-        tours: { $push: '$name' }
-      }
-    },
-    {
-      $addFields: { month: '$_id' }
-    },
-    {
-      $project: {
-        _id: 0
-      }
-    },
-    {
-      $sort: { numTourStarts: -1 }
-    },
-    {
-      $limit: 12
-    }
-  ]);
+//   const plan = await Tour.aggregate([
+//     {
+//       $unwind: '$startDates'
+//     },
+//     {
+//       $match: {
+//         startDates: {
+//           $gte: new Date(`${year}-01-01`),
+//           $lte: new Date(`${year}-12-31`)
+//         }
+//       }
+//     },
+//     {
+//       $group: {
+//         _id: { $month: '$startDates' },
+//         numTourStarts: { $sum: 1 },
+//         tours: { $push: '$name' }
+//       }
+//     },
+//     {
+//       $addFields: { month: '$_id' }
+//     },
+//     {
+//       $project: {
+//         _id: 0
+//       }
+//     },
+//     {
+//       $sort: { numTourStarts: -1 }
+//     },
+//     {
+//       $limit: 12
+//     }
+//   ]);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      plan
-    }
-  });
-});
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       plan
+//     }
+//   });
+// });
 
 // /tours-within/:distance/center/:latlng/unit/:unit
 // /tours-within/233/center/34.111745,-118.113491/unit/mi
